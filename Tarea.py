504ae4d6de1b2b1a3dcf13fa699b5452e595da27
CesarPalomeque <85848931+CesarPalomeque@users.noreply.github.com>
2021-07-17 16:55:26 -0500

    Create Tarea.py

---
+++ Tarea.py
@@ -0,0 +1,31 @@
+class Tarea:
+
+    def __init__(self):
+        pass
+    def calcularJornada(self):
+
+        ht, he, het= 0,0,0
+        ph, phe, pt= 0,0,0
+
+        ht = int(input("Ingrese horas trabajadas: "))
+        ph = float(input("Ingrese valor hora: "))
+
+        if ht > 40:
+            he = ht-40
+            if he > 8:
+                het = he - 8
+                ph8 = 8*ph*2
+                phe = het*ph*3
+            else:
+                phe = 0
+                ph8 = he*ph*2    
+            pt = 40*ph + ph8 + phe
+        else:
+            ph8=0
+            pt = ht*ph
+        print("Sobretiempo<8:{}, Sobretiempo>8:{}, Jornada:{}".format(ph8,phe,pt))
+
+
+
+tarea=Tarea()
+tarea.calcularJornada()
