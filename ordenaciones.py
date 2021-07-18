da56feb1323ce525b947b3a34499e43a2926265f
CesarPalomeque <85848931+CesarPalomeque@users.noreply.github.com>
2021-07-17 19:30:20 -0500

    Update ordenaciones.py

--- ordenaciones.py
+++ ordenaciones.py
@@ -3,28 +3,28 @@ class Ordenar:
     def __init__(self,lista):
         self.lista=lista
         
-    # def recorrerElemento(self):
-    #     for ele in self.lista:
-    #         print(ele)
+    def recorrerElemento(self):
+        for ele in self.lista:
+            print(ele)
             
-    # def recorrerPosicion(self):
-    #     for pos,ele in enumerate(self.lista):
-    #         print(pos,ele)
+    def recorrerPosicion(self):
+        for pos,ele in enumerate(self.lista):
+            print(pos,ele)
     
-    # def recorrerRange(self):
-    #     for pos in range(len(self.lista)):
-    #         print(pos,self.lista[pos])
+    def recorrerRange(self):
+        for pos in range(len(self.lista)):
+            print(pos,self.lista[pos])
             
-    # def buscar (self,buscado):
-    #     enc=False
-    #     for pos,ele in enumerate(self.lista):
-    #         if ele == buscado:
-    #             enc=True
-    #             break
-    #     if enc == True:
-    #         return pos
-    #     else:
-    #         return -1
+    def buscar (self,buscado):
+        enc=False
+        for pos,ele in enumerate(self.lista):
+            if ele == buscado:
+                enc=True
+                break
+        if enc == True:
+            return pos
+        else:
+            return -1
     
     def ordenarAsce(self):
         for pos in range(0,len(self.lista)):
@@ -33,6 +33,9 @@ class Ordenar:
                     aux = self.lista[pos]
                     self.lista[pos]= self.lista[sig]
                     self.lista[sig]= aux
+
+
+
 
            
 lista = [2,3,1,5,8,10]
